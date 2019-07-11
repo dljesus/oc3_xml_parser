c0c57f535415e02d2367283cd4180e6b132a1f42
dlJesus <dimazavr93@gmail.com>
2019-07-11 18:49:19 +0300

    dsa

--- system/library/suppliers/YandexYmlParser.php
+++ system/library/suppliers/YandexYmlParser.php
@@ -24,15 +24,52 @@ class YandexYmlParser extends AbstractParser
         $matches = array();
         preg_match_all($regexOffer , $this->feed, $matches);
         foreach ($matches as $match) {
-            $productId = $match['1'];
-            $productName = $this->getName($match['1']);
+            $id = $match['1'];
+            $price = $this->getPrice($match['0']);
+            $name = $this->getName($match['0']);
+            $description = $this->getDescription($match[0]);
+            $category = $this->getProductCategory($match[0]);
+            $images = $this->getImages($match[0], $name);
+        }
+    }
+
+    public function getImages($offer, $name){
+        $images = array();
+        $file = $this->translit($name) . ;
+        $path = DIR_IMAGE . 'catalog/' . $file{0} . '/' . $file{1};
+        $regex = '/<picture>(.*?)<\/picture>/sui';
+        $matches = array();
+        preg_match_all ($regex, $offer, $matches, PREG_SET_ORDER);
+        foreach ($matches as $key => $match) {
+            $image = $this->downloadImage($match[1], $fileName);
+
         }
 
-
-        // TODO: Implement getProduct() method.
     }
 
     protected function getName($offer){
+        $regex = '/<name>(.*?)<\/name>/sui';
+        $match = array();
+        preg_match ($regex, $offer, $match);
+        return $match['1'];
+    }
 
+    protected function getPrice($offer){
+        $regex = '/<price>(.*?)<\/price>/sui';
+        $match = array();
+        preg_match ($regex, $offer, $match);
+        return $match['1'];
+    }
+    protected function getProductCategory($offer){
+        $regex = '/<categoryId>(.*?)<\/categoryId>/sui';
+        $match = array();
+        preg_match ($regex, $offer, $match);
+        return $match['1'];
+    }
+    protected function getDescription($offer){
+        $regex = '/<description>(?:<!\[CDATA\[|)(.*?)(?:\]\]>|)<\/description>/sui';
+        $match = array();
+        preg_match ($regex, $offer, $match);
+        return trim($match['1']);
     }
 }
